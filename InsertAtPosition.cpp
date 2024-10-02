b94832ea2a30b8929663726c078ee895073f2f55
MukuSick2 <131860691+MukuSick2@users.noreply.github.com>
2024-10-02 23:00:40 +0530

    Add files via upload

---
+++ InsertAtPosition.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(){}
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+    Node(int data,Node* next){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+void InsertAtTail(Node* &tail,int a){
+    // Node* &tail => by reference coz we dont want to create a copy of it and work //
+    Node* temp = new Node(a);
+    tail->next=temp;
+    tail = temp;  // we can alos write tail = tail -> next;
+}
+
+void InsertAtPosition(Node* &head,int pos, int a){
+    // for traversing till n-1th position and storing its value, we use: //
+    Node* temp = head;
+    for(int i=1;i<pos-1;i++){
+        temp=temp->next;
+    }
+
+    // for storing new value , we need: //
+    Node* ins = new Node(a);
+
+    ins->next = temp->next; // for giving address of next value to new value //
+    temp->next = ins;      // for given address of new value to n-1th position / prev. value / 
+
+
+}
+void display(Node* &tail){
+    Node* temp=tail;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp= temp->next;
+    }
+    cout<<endl;
+
+} 
+
+
+int main(){
+    Node* node1 = new Node(10);
+    cout<< node1->data<<endl<<node1->next<<endl;
+
+    Node* head = node1;
+    Node* tail = node1;
+
+    display(head);
+    InsertAtTail(tail,12);
+    display(head);
+    InsertAtTail(tail,13);
+    display(head);
+    InsertAtPosition(head,4,40);
+    display(head);
+
+}
+
+// output => 10 
+//           0
+//           10
+//           10 12
+//           10 12 13
+
